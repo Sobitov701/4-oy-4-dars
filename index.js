58d5d06b72a9144a28579c946b8c5c07b3f85e71
Sobitov701 <abdurasul03sobitaliyev>
2024-11-30 21:35:12 +0300

    all add

---
+++ index.js
@@ -0,0 +1,414 @@
+for (let num1 = 1; num1 <= 100; num1++) {
+  console.log(num1);
+}
+
+for (let num2 = 1; num2 <= 50; num2++) {
+  if (num2 % 2 == 0) {
+    console.log(num2);
+  }
+}
+
+for (let num3 = 1; num3 <= 30; num3++) {
+  if (num3 % 2 == 1) {
+    console.log(num3);
+  }
+}
+
+for (let num4 = 1; num4 <= 20; num4++) {
+  console.log(num4 ** 2);
+}
+
+for (let num5 = 10; num5 >= 1; num5--) {
+  console.log(num5);
+}
+
+let sum = 0;
+
+for (let num6 = 1; num6 <= 100; num6++) {
+  if ((sum = num6 + sum)) {
+  }
+}
+
+console.log(sum);
+
+let max = 0;
+
+for (let num7 = 1; num7 <= 20; num7++) {
+  if (num7 % 2 == 0) {
+    max = max + num7;
+  }
+}
+
+console.log(max);
+
+let min = 0;
+
+for (let num8 = 1; num8 <= 15; num8++) {
+  if (num8 % 2 == 1) {
+    min = min + num8;
+  }
+}
+
+console.log(min);
+
+for (let num9 = 5; num9 <= 25; num9++) {
+  if (num9) {
+    console.log(num9 ** 3);
+  }
+}
+
+for (let num10 = 0; num10 < 10; num10++) {
+  console.log(num10 ** (1 / 2));
+}
+
+for (let num11 = 1; num11 < 50; num11++) {
+  if (num11 % 3 == 0) {
+    console.log(num11);
+  }
+}
+
+let a = 0;
+
+for (let num12 = 1; num12 < 100; num12++) {
+  if ((a = a + num12)) {
+  }
+}
+
+console.log(a);
+
+for (let num13 = 1; num13 <= 100; num13++) {
+  if (num13 % 7 == 0) {
+    console.log(num13);
+  }
+}
+
+let b = 0;
+
+for (let num14 = 1; num14 <= 100; num14++) {
+  if (num14) {
+    b = b + num14;
+  }
+}
+
+console.log(b / 2);
+
+let n = 0;
+
+for (let num15 = 1; num15 < 20; num15++) {
+  if (num15 % 2 == 1) {
+    n = n + num15;
+  }
+}
+
+console.log(n);
+
+let num16 = +prompt("sonni kirting");
+
+for (let num_16 = 0; num_16 < num16; num_16++) {
+  if (num_16 % 2 == 0) {
+    console.log(num_16);
+  }
+}
+
+for (let num17 = 1; num17 < 50; num17++) {
+  if (num17 % 3 == 0 && num17 % 5 == 0) {
+    console.log(num17);
+  }
+}
+
+let num_ = 0;
+
+for (let num18 = 1; num18 <= 30; num18++) {
+  if (num18) {
+    num18 ** 2;
+    num_ = num18 + num_;
+  }
+}
+
+console.log(num_);
+
+let m = 0;
+
+for (let num19 = 1; num19 < 100; num19++) {
+  if (num19 % 2 == 1) {
+    console.log(num19);
+    m = num19 + m;
+  }
+}
+
+console.log(m);
+
+let num20 = +prompt("sonni kiriting");
+let _num20 = +prompt("sonni kiriting");
+
+let ma_x;
+let mi_n;
+
+if (num20 > _num20) {
+  ma_x = num20;
+  mi_n = _num20;
+} else {
+  ma_x = _num20;
+  mi_n = num20;
+}
+
+let su_m = 0;
+
+for (let nu_m20 = mi_n; nu_m20 < ma_x; nu_m20++) {
+  su_m += nu_m20;
+}
+
+console.log(su_m);
+
+for (let num21 = 1; num21 < 50; num21++) {
+  if (num21 % 3 == 0) {
+    console.log(num21);
+  }
+}
+
+for (let num22 = 1; num22 <= 20; num22++) {
+  if (num22 % 2 == 1) {
+    console.log(num22);
+  } else if (num22 % 2 == 0) {
+    console.log("juft son");
+  }
+}
+
+let g = 0;
+
+for (let num23 = 1; num23 < 100; num23++) {
+  if (num23 % 10 != 0) {
+    g = num23 + g;
+  }
+}
+
+console.log(g);
+
+let f = 1;
+
+for (let num24 = 1; num24 < 30; num24++) {
+  if (num24) {
+    f = f * num24;
+  }
+}
+
+console.log(f);
+
+let num25 = promt("son");
+let $num25 = promt("son");
+
+for (let num_25 = num25; num_25 <= $num25; num_25++) {
+  console.log(num_25);
+}
+
+for (let num26 = 1; num26 <= 100; num26++) {
+  if (num26 % 3 == 0 && num26 % 5 == 0) {
+    console.log(num26);
+  }
+}
+
+let num27 = promt("son");
+
+let factorial = 1;
+
+for (let $num27 = 1; $num27 < num27; $num27++) {
+  factorial *= $num27;
+}
+
+console.log(factorial);
+
+let k = 0;
+
+for (let num28 = 1; num28 < 50; num28++) {
+  if (num28 % 2 == 0) {
+    k = num28 + k;
+  }
+}
+
+console.log(k);
+
+for (let num29 = 1; num29 <= 10; num29++) {
+  if (num29) {
+    console.log(num29 ** 3);
+  }
+}
+
+for (let num30 = 1; num30 <= 20; num30++) {
+  console.log(`${num30} - Bu sonning kvadrati: ${num30 * num30}`);
+}
+
+let num1 = 1;
+
+for (let num31 = 1; num31 <= 100; num31++) {
+  if (num31 % 7 == 0) {
+    num1 = num1 * num31;
+  }
+}
+
+console.log(num1);
+
+for (let num32 = 1; num32 < 30; num32++) {
+  if (num32) {
+    console.log(num32 * 10);
+  }
+}
+
+for (let num33 = 1; num33 <= 20; num33++) {
+  console.log(num33);
+}
+
+let number = prompt("son");
+
+let $sum = 0;
+
+for (let num34 = 0; num34 < number; num34++) {
+  if (num34 % 2 === 0) {
+    $sum += num34;
+  }
+}
+
+console.log($sum);
+
+let num35 = +prompt("son ");
+let $num35 = +prompt("son ");
+
+let num_35 = 1;
+
+let start_max;
+let end_min;
+
+if (num35 > $num35) {
+  start_max = num35;
+  end_min = $num35;
+} else {
+  start_max = $num35;
+  end_min = num35;
+}
+
+for (let _num35 = start_max; _num35 <= end_min; _num35++) {
+  num_35 *= _num35;
+}
+
+console.log(num_35);
+
+let num36 = +prompt("son ");
+
+for (let $num36 = num36 + 1; $num36 <= num36 + 100; $num36++) {
+  if ($num36 % 10 === 0) {
+    console.log($num36);
+  }
+}
+
+let $num37 = 0;
+
+for (let num37 = 1; num37 < 15; num37++) {
+  $num37 += num37 ** 2;
+}
+
+console.log($num37);
+
+for (let num38 = 1; num38 <= 10; num38++) {
+  let factorial = 1;
+  for (let $num38 = 1; $num38 <= num38; $num38++) {
+    factorial *= $num38;
+  }
+  console.log(factorial);
+}
+
+let $num39 = prompt("musbat sonni kirting");
+
+let s$um = 0;
+
+for (let num39 = 1; num39 < $num39; num39++) {
+  s$um += num39 ** 2;
+}
+
+console.log(s$um);
+
+let num$40 = promt("son");
+
+for (let num40 = num$40; num40 < 1; num40--) {
+  console.log(num40);
+}
+
+let $num41 = 0;
+
+for (let num41 = 1; num41 < 50; num41++) {
+  if (num41 % 2 == 0) {
+    $num41 = num41 ** 3;
+    console.log($num41);
+  }
+}
+
+for (let num42 = 1; num42 <= 100; num42++) {
+  if (num42 % 5 != 0) {
+    console.log(num42);
+  }
+}
+
+let _num43 = +prompt("musbat son kirting");
+let $num43 = 0;
+
+for (let num43 = 1; num43 < _num43; num43++) {
+  if (num43 % 3 == 0) {
+    $num43 += num43;
+  }
+}
+
+console.log($num43);
+
+for (let num44 = 1; num44 < 20; num44++) {
+  if (num44 % 2 == 0) {
+    console.log("juft son");
+  } else {
+    console.log("toq son");
+  }
+}
+
+let $num45 = 0;
+
+for (let num45 = 0; num45 < 10; num45++) {
+  if (num45 % 2 == 1) {
+    $num45 += num45 ** 2;
+  }
+}
+
+console.log($num45);
+
+let $num47 = 0;
+
+for (let num47 = 1; num47 < 30; num47++) {
+  console.log(num47);
+  if (num47 % 3 == 0 && num47 % 4 == 0) {
+    $num47 += num47;
+  }
+}
+
+console.log($num47);
+
+for (let num48 = 1; num48 < 50; num48++) {
+  if (num48 % 2 == 1) {
+    console.log(num48);
+  }
+}
+
+let $num49 = +prompt("Son kiriting:");
+let kopaytmasi = 1;
+
+for (let num49 = 7; num49 < $num49; num49 += 7) {
+  kopaytmasi *= num49;
+}
+
+console.log(kopaytmasi);
+
+let boshla = +prompt("boshlang'ich sonni kirtin");
+let Tugat = +prompt("tugash sonini kirting");
+let $num50 = 0;
+let kvadrat;
+
+for (let num50 = boshla; num50 <= Tugat; num50++) {
+  kvadrat = num50 ** 2;
+  sum += kvadrat;
+}
+
+console.log($num50);
